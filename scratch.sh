f3a15c923012804ab07ce3e4b30cd3c34a7c413f
Andrew Seward <awswrd@gmail.com>
2021-12-05 16:53:47 -0800

    Continue on tooling started in 59c1067

--- scratch.sh
+++ scratch.sh
@@ -2,26 +2,69 @@
 
 set -euo pipefail
 
-readonly dir_path="$1"
+help() {
+  # shellcheck disable=SC2016
+  echo 'Functions:
 
-if [ -f "${dir_path}/deprecationMessage.txt" ]; then
-  dep_msg_expr="Some (./deprecationMessage.txt as Text)"
-else
-  dep_msg_expr='None Text'
-fi
+  For inputs:
+  - gen_input_pkg <input_name>
+  - write_input_package <input_name>
+  - write_input_packages_all
 
-if [ -f "${dir_path}/default.dhall" ]; then
-  default_expr="Some ./default.dhall"
-else
-  default_expr="None JSON.Type"
-  readonly json_expr="${2:-"$(yaml-to-dhall <<< '[{}, 0]' | dhall type | tail -n +2)"}"
-  echo "let JSON = ${json_expr} in"
-fi
-
-cat <<< "
-{ default            = ${default_expr}
-, deprecationMessage = ${dep_msg_expr}
-, description        = ./description as Text
-, required           = ./required.dhall
+  For `action.yml`:
+  - gen_action_yml
+  - write_action_yml
+'
 }
-" | dhall lint > "${dir_path}/package.dhall"
+
+gen_input_pkg() {
+  readonly input_name="$1"
+  readonly dir_path="./inputs/${input_name}"
+
+  if [ -f "${dir_path}/deprecationMessage.txt" ]; then
+    readonly dep_msg_expr="Some (./deprecationMessage.txt as Text)"
+  else
+    readonly dep_msg_expr='None Text'
+  fi
+
+  if [ -f "${dir_path}/default.dhall" ]; then
+    default_expr="Some ./default.dhall"
+  else
+    default_expr="None JSON.Type"
+    readonly json_expr="${2:-"$(yaml-to-dhall <<< '[{}, 0]' | dhall type | tail -n +2)"}"
+    echo "let JSON = ${json_expr} in"
+  fi
+
+  cat <<< "
+  { default            = ${default_expr}
+  , deprecationMessage = ${dep_msg_expr}
+  , description        = ./description as Text
+  , required           = ./required.dhall
+}
+" | dhall lint # > "${dir_path}/package.dhall"
+}
+
+write_input_package() {
+  readonly input_name="$1"
+  readonly dir_path="./inputs/${input_name}"
+
+  "$0" gen_input_pkg "${input_name}" > "${dir_path}/package.dhall"
+}
+
+write_input_packages_all() {
+  # shellcheck disable=SC2038
+  find ./inputs -mindepth 1 -maxdepth 1 -type d \
+    | xargs -n1 basename \
+    | xargs -t -n1 "$0" write_input_package
+}
+
+gen_action_yml() {
+  # shellcheck disable=SC2068
+  dhall-to-yaml --omit-empty $@ <<< ./action.yml.dhall
+}
+
+write_action_yml() {
+  "$0" gen_action_yml --output ./action.yml --generated-comment
+}
+
+"${@:-help}"
